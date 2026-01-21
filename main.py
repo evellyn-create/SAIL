73ee8b60b7d48db09816c34fbf151b5d6fd8de07
Evellyn dos Santos Ribeiro <evellyn.r@academico.ifrn.edu.br>
2026-01-21 11:24:12 -0300

    atualizando proj

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
-class Lixo():
-    def __init__(self, tipo, categoria):
-       self.tipo = tipo
-       self.categoria = categoria
-       self.lista_lixo = []
+import time
 
-    def mostrar_lixo_categoria(self, tipo, categoria):
-        
+# class Lixo():
+#     def __init__(self, tipo, categoria):
+#        self.tipo = tipo
+#        self.categoria = categoria
+#        self.lista_lixo = []
 
-    def listagem(self, lista_lixo):
-        item = input('Adicione os itens que vão ser jogados fora: ')
-        lista_lixo.append(item)
-        return lista_lixo
+    # def mostrar_lixo_categoria(self, tipo, categoria):
+
+    # def listagem(self, lista_lixo):
+    #     item = input('Adicione os itens que vão ser jogados fora: ')
+    #     lista_lixo.append(item)
+    #     return lista_lixo
+    #     
     
-class Reciclavel(Lixo):
-    def __init__(self, tipo, categoria):
-        super().__init__(tipo, categoria)
+# class Reciclavel(Lixo):
+#     def __init__(self, tipo, categoria):
+#         super().__init__(tipo, categoria)
 
-    def
+
+class Menu():
+    print('=' * 20)
+    print('SAIL')
+    print('=' * 20)
+    time.sleep(2)
+
+    print('Bem-vindo(a) ao SAIL!')
+
+    def menu_principal(self):
+        print('''Escolha o serviço que o SAIL pode fazer por você:
+              (1) mostrar categoria do resíduo
+              (2) gerar lista de resíduos
+              (3) rrge
+              (4) ''')
+
+# m = Menu()
+# print(m)
+    
+
+
+
+    
 
     
